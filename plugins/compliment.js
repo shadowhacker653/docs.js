0214e1f80190ec23bc7a053cbe0e8c27bb2d8834
Jashin653 <35552297+shadowhacker653@users.noreply.github.com>
2021-05-27 10:08:06 +0530

    Create compliment.js

---
+++ plugins/compliment.js
@@ -0,0 +1,18 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
+
+Asena.addCommand({pattern: 'compliment', fromMe: false, desc: Lang.CM_DESC}, async (message, match) => {
+	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+	const url = `https://complimentr.com/api`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🤗 ' + Lang.CM +'* ```' + json.compliment + '```\n\n' , MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
+	}
+});
